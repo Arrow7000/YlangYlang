79e42544c1859cae34da67427f13b29caa4a2e25
Aron Adler <aron@adler.dev>
2023-09-30 18:52:04 +0100

    Implement disjoint-set forest
    - both plain and with ability to include data
    - and start writing some tests
    - including a dummy type system for testing data unification

---
+++ YlangYlang.Tests/DisjointSetForestTests.fs
@@ -0,0 +1,267 @@
+﻿module DisjointSetForestTests
+
+open DisjointSetForest
+
+open Expecto
+
+
+let fail = Expecto.Tests.failtest
+
+type Items =
+    | A
+    | B
+    | C
+    | D
+    | E
+    | F
+    | G
+    | H
+    | I
+    | J
+
+
+
+
+
+
+
+[<Tests>]
+let testDisjointSetForest =
+    testList
+        "Disjoint set forests"
+        [ testList
+              "Simple DSF containing only sets"
+              [ testCase "Add disjoint set"
+                <| fun _ ->
+                    let dsf = DSF.init [ A; B; C ] |> DSF.addSet [ D; E; F ]
+                    let resultA = DSF.find A dsf
+                    let resultB = DSF.find B dsf
+
+                    let resultD = DSF.find D dsf
+
+                    Expect.isSome resultA "resultA can be found in the DSF"
+                    Expect.isSome resultB "resultB can be found in the DSF"
+                    Expect.isSome resultD "resultD can be found in the DSF"
+
+                    Expect.equal resultA resultB "A and B are in the same set"
+                    Expect.notEqual resultA resultD "A and D are not in the same set"
+                    Expect.notEqual resultB resultD "B and D are not in the same set"
+
+
+                testCase "Union set"
+                <| fun _ ->
+                    let dsf =
+                        DSF.init [ A; B; C ]
+                        |> DSF.addSet [ D; E; F ]
+                        |> DSF.addSet [ H; I; J ]
+                        |> DSF.union A E
+
+                    let resultA = DSF.find A dsf
+                    let resultD = DSF.find D dsf
+
+                    let resultH = DSF.find H dsf
+
+                    Expect.isSome resultA "resultA can be found in the DSF"
+                    Expect.isSome resultD "resultD can be found in the DSF"
+                    Expect.isSome resultH "resultH can be found in the DSF"
+
+                    Expect.equal resultA resultD "A and D are in the same set"
+                    Expect.notEqual resultA resultH "A and H are not in the same set"
+                    Expect.notEqual resultD resultH "D and H are not in the same set"
+
+                ]
+
+          ]
+
+
+type Name =
+    | Foo
+    | Bar
+    | Baz
+    | Qux
+    | Corge
+    | Grault
+    | Garply
+    | Waldo
+    | Fred
+    | Plugh
+    | Xyzzy
+    | Thud
+
+/// Representing a type that's either constrained to be the same type as a particular named value or not constrained at all
+type NamedOrAny =
+    /// For types that are constrained to be the same type as some name
+    | Named of Name
+    /// For types that are not constrained to any name
+    | Any
+
+
+/// Simplified version of the Elm type system, for ease of testing
+type SimpleType =
+    | Str
+    | Int
+    | List of NamedOrAny
+    | Pair of NamedOrAny * NamedOrAny
+
+
+and SimpleResult = Result<SimpleType, unit>
+and SimpleResultAndItems = SimpleResult * NamedOrAny set
+
+
+
+/// Simplified type unification logic to be used in DSFC tests
+let unifySimpleTypes
+    (typeResultA : Result<SimpleType, unit>)
+    (typeResultB : Result<SimpleType, unit>)
+    : SimpleResult * Name seq =
+    let rec unifyTypes typeResultA typeResultB =
+        match typeResultA, typeResultB with
+        | Ok typeA, Ok typeB ->
+            match typeA, typeB with
+            | Str, Str -> Ok Str, Seq.empty
+            | Int, Int -> Ok Int, Seq.empty
+            | List typeParamA, List typeParamB ->
+                let unifiedNamedsOrAnys, names = unifyNamedOrAnys typeParamA typeParamB
+                Ok (List unifiedNamedsOrAnys), names
+
+            | Pair (fstA, sndA), Pair (fstB, sndB) ->
+                let unifiedNamedsOrAnysFst, namesFst = unifyNamedOrAnys fstA fstB
+                let unifiedNamedsOrAnysSnd, namesSnd = unifyNamedOrAnys sndA sndB
+
+                Ok (Pair (unifiedNamedsOrAnysFst, unifiedNamedsOrAnysSnd)), Seq.append namesFst namesSnd
+
+            | _, _ -> Error (), Seq.empty
+
+        | Error e, _
+        | _, Error e -> Error e, Seq.empty
+
+    and unifyNamedOrAnys typeParamA typeParamB =
+        match typeParamA, typeParamB with
+        | Any, other
+        | other, Any -> other, Seq.empty
+        | Named nameA, Named nameB ->
+            // We pick one of the two names, but we bubble up the fact that both these names need to be unified by returning the seq containing both names to the caller
+            Named nameA,
+            if nameA = nameB then
+                Seq.empty
+            else
+                seq {
+                    nameA
+                    nameB
+                }
+
+    unifyTypes typeResultA typeResultB
+
+
+let unifySimpleTypesMany (simpleTypesNel : Result<SimpleType, unit> nel) : SimpleResult * Name seq =
+    let (NEL (head, tail)) = simpleTypesNel
+
+    tail
+    |> List.fold
+        (fun (combinedTypeResult, names) item ->
+            let newResult, newNames = unifySimpleTypes combinedTypeResult item
+            newResult, Seq.append names newNames)
+        (head, Seq.empty)
+
+
+
+
+[<Tests>]
+let testSimpleTypeUnification =
+    testList
+        "Unification logic for SimpleTypes"
+        [ testCase "Unify the same primitives"
+          <| fun () ->
+              let unifiedStrs, namesToUnifyForStr = unifySimpleTypes (Ok Str) (Ok Str)
+              Expect.equal unifiedStrs (Ok Str) "Unifying two strings -> string"
+              Expect.equal namesToUnifyForStr Seq.empty "No names to unify for String unification"
+
+
+              let unifiedInts, namesToUnifyForInt = unifySimpleTypes (Ok Int) (Ok Int)
+              Expect.equal unifiedInts (Ok Int) "Unifying two Ints -> Int"
+              Expect.equal namesToUnifyForInt Seq.empty "No names to unify for Int unification"
+
+
+          testCase "Unify incompatible types"
+          <| fun () ->
+              let intWithStr, _ = unifySimpleTypes (Ok Int) (Ok Str)
+              Expect.equal intWithStr (Error ()) "Int =/= Str"
+
+              let intWithList, _ = unifySimpleTypes (Ok Int) (Ok (List Any))
+              Expect.equal intWithList (Error ()) "Int =/= List _"
+
+              let intWithPair, _ = unifySimpleTypes (Ok Int) (Ok (Pair (Named Foo, Any)))
+              Expect.equal intWithPair (Error ()) "Int =/= (foo, _)"
+
+
+          testCase "Unify List type with params"
+          <| fun () ->
+              let unifiedType, namesToUnify =
+                  unifySimpleTypes (Ok (List (Named Foo))) (Ok (List Any))
+
+              Expect.equal unifiedType (Ok (List (Named Foo))) "Narrows to more specific type param"
+              Expect.isEmpty namesToUnify "No names left to unify"
+
+          testCase "Unify Pair type with params"
+          <| fun () ->
+              let unifiedType, namesToUnify =
+                  unifySimpleTypes (Ok (Pair (Named Foo, Any))) (Ok (Pair (Any, Named Bar)))
+
+              Expect.equal unifiedType (Ok (Pair (Named Foo, Named Bar))) "Narrows to more specific type param"
+              Expect.isEmpty namesToUnify "No names left to unify"
+
+
+          testCase "Unify Pair type with named params"
+          <| fun () ->
+              let unifiedType, namesToUnify =
+                  unifySimpleTypes (Ok (Pair (Named Foo, Any))) (Ok (Pair (Any, Named Bar)))
+
+              Expect.equal unifiedType (Ok (Pair (Named Foo, Named Bar))) "Narrows to more specific type param"
+              Expect.isEmpty namesToUnify "No names left to unify"
+
+
+          testCase "Unify Pair type with multiple named params"
+          <| fun () ->
+              let unifiedType, namesToUnify =
+                  unifySimpleTypes (Ok (Pair (Named Foo, Any))) (Ok (Pair (Named Bar, Any)))
+
+              Expect.isOk unifiedType "Can be unified"
+              Expect.sequenceEqual (Seq.sort namesToUnify) (Seq.sort [ Foo; Bar ]) "Need to unify two names"
+
+          ]
+
+
+[<Tests>]
+let testDisjointForestSetWithData =
+    testList
+        "DSFs with data"
+        [ testCase "Set union results"
+          <| fun () ->
+              let dsf =
+                  DSFC.init unifySimpleTypesMany [ Foo; Bar ] (Ok (List Any))
+                  |> DSFC.addSet (Ok (List (Named Qux))) [ Bar; Baz ]
+
+              let firstSetRoot =
+                  Expect.wantSome (DSFC.findSetRoot Foo dsf) "Foo should be in the DSF"
+
+              let secondSetRoot =
+                  Expect.wantSome (DSFC.findSetRoot Baz dsf) "Baz should be in the DSF"
+
+              Expect.equal firstSetRoot secondSetRoot "The two sets should be the same now"
+
+
+          testCase "Set union results"
+          <| fun () ->
+              let dsf =
+                  DSFC.init unifySimpleTypesMany [ Foo; Bar ] (Ok (List Any))
+                  |> DSFC.addSet (Ok (List (Named Qux))) [ Bar; Baz ]
+                  |> DSFC.addSet (Ok (Pair (Named Corge, Named Bar))) [ Qux ] // I think... this results in an infinite type elaboration. Which, to be clear, may not be an issue for this dummy type system that can't actually expand the names, but ~would be an issue for a real type system, where we'd need to have some sanity checks in place to ensure that such infinitely deep recursive types are not allowed~ COULD be an issue for a real type system. But actually maybe also not. It may only be an issue for the developer because they could construct a type whose only valid *values* would need to be infinitely deep, but maybe that's not something we the language implementers need to care about? idk. It should probably be a warning though.
+              // Where this *wouldn't* be a problem at all though is where the type referenced is a sum type, meaning that even though following some branches leads to infinite depth recursion, actually it *is* possible to create non-infinitely deep values that satisfy the type, so that would be all cool and good.
+
+              let firstSetRoot =
+                  Expect.wantSome (DSFC.findSetRoot Foo dsf) "Foo should be in the DSF"
+
+              let secondSetRoot =
+                  Expect.wantSome (DSFC.findSetRoot Baz dsf) "Baz should be in the DSF"
+
+              Expect.equal firstSetRoot secondSetRoot "The two sets should be the same now" ]
---
+++ YlangYlang/DisjointSetForest.fs
@@ -0,0 +1,310 @@
+﻿module DisjointSetForest
+
+
+
+
+type DisjointSetForest<'T when 'T : comparison> =
+    { map : Map<'T, 'T option> }
+
+    static member init (elems : 'T seq) : DisjointSetForest<'T> =
+        let root = Seq.min elems
+        let nonRoots = Seq.filter ((<>) root) elems
+
+        { map =
+            nonRoots
+            |> Seq.map (fun elem -> elem, Some root)
+            |> Map.ofSeq
+            |> Map.add root None }
+
+    static member find (elem : 'T) (dsf : DisjointSetForest<'T>) =
+        match Map.tryFind elem dsf.map with
+        | None -> None
+        | Some found ->
+            match found with
+            | None -> Some elem
+            | Some result -> DisjointSetForest.find result dsf
+
+    /// This also adds the items into the DSF if they're not there yet
+    static member union (a : 'T) (b : 'T) (dsf : DisjointSetForest<'T> when 'T : comparison) =
+        let rootOfAOpt = DSF.find a dsf
+        let rootOfBOpt = DSF.find b dsf
+
+        match rootOfAOpt, rootOfBOpt with
+        | None, None ->
+            let minItem = min a b
+            let maxItem = max a b
+
+            { map =
+                dsf.map
+                |> Map.add maxItem (Some minItem)
+                |> Map.add minItem None }
+
+        | Some rootOfA, None ->
+            let minItem = min rootOfA b
+            let maxItem = max rootOfA b
+
+            { map =
+                dsf.map
+                |> Map.add maxItem (Some minItem)
+                |> Map.add minItem None }
+
+        | None, Some rootOfB ->
+            let minItem = min a rootOfB
+            let maxItem = max a rootOfB
+
+            { map =
+                dsf.map
+                |> Map.add maxItem (Some minItem)
+                |> Map.add minItem None }
+
+        | Some rootOfA, Some rootOfB ->
+            let minItem = min rootOfA rootOfB
+            let maxItem = max rootOfA rootOfB
+
+            { map = dsf.map |> Map.add maxItem (Some minItem) }
+
+    static member addSet (elems : 'T seq) (dsf : DisjointSetForest<'T>) =
+        let rootsOrNewElems =
+            elems
+            |> Seq.map (fun elem ->
+                // Get either the root node if elem is in the map, or get the elem itself if it's not
+                DSF.find elem dsf |> Option.defaultValue elem)
+
+        /// Get the min item of the whole thing, either the found root nodes, or the elems themselves
+        let minItem = Seq.min rootsOrNewElems
+
+        let newMap =
+            rootsOrNewElems
+            |> Seq.fold
+                (fun map elem ->
+                    // Add or replace the elems with a reference to the new smallest item, which will therefore be the newest root node for all items in the combined set
+                    Map.add elem (Some minItem) map)
+                dsf.map
+            |> Map.add minItem None
+
+        { map = newMap }
+
+
+and DSF<'T when 'T : comparison> = DisjointSetForest<'T>
+
+
+
+
+
+
+
+
+
+
+
+
+
+type PointerOrContent<'T, 'U when 'T : comparison> =
+    | Pointer of 'T
+    | Content of 'U
+
+
+type DisjointSetForestWithContent<'T, 'U when 'T : comparison and 'U : equality> =
+    { map : Map<'T, PointerOrContent<'T, 'U>>
+
+      /// This is just to store a function, it doesn't actually do anything else.
+      ///
+      /// This is the function that, given two pieces of data and the DSF, returns the unified piece of data plus which (if any) elems now need to be unioned
+      unifyDataMany : 'U nel -> 'U * 'T seq }
+
+    static member init
+        (unifyDataMany : 'U nel -> 'U * 'T seq)
+        (elems : 'T seq)
+        (data : 'U)
+        : DisjointSetForestWithContent<'T, 'U> =
+        let root = Seq.min elems
+        let nonRoots = Seq.filter ((<>) root) elems
+
+        { map =
+            nonRoots
+            |> Seq.map (fun elem -> elem, Pointer root)
+            |> Map.ofSeq
+            |> Map.add root (Content data)
+          unifyDataMany = unifyDataMany }
+
+    static member findRootAndData (elem : 'T) (dsf : DSFC<'T, 'U>) : ('T * 'U) option =
+        match Map.tryFind elem dsf.map with
+        | None -> None
+        | Some found ->
+            match found with
+            | Content data ->
+                // Then this must be the root element of the set
+                Some (elem, data)
+
+            | Pointer ptr -> DSFC.findRootAndData ptr dsf
+
+
+    /// This gives you the root element of the set, so if two elements have the same root element they are in the same set
+    static member findSetRoot (elem : 'T) (dsf : DSFC<'T, 'U>) : 'T option =
+        DSFC.findRootAndData elem dsf |> Option.map fst
+
+
+    /// This gives you the data for the set that this element is in
+    static member findData (elem : 'T) (dsf : DSFC<'T, 'U>) : 'U option =
+        DSFC.findRootAndData elem dsf |> Option.map snd
+
+
+
+
+
+
+    static member private unionSetsSingleStepMany
+        (elemsToMerge : 'T seq)
+        (dsf : DSFC<'T, 'U>)
+        : 'U nel * 'T * DSFC<'T, 'U> =
+
+        let findResults =
+            elemsToMerge
+            |> Seq.map (fun elem -> elem, DSFC.findRootAndData elem dsf)
+
+        let newElems, existingElems =
+            findResults
+            |> Seq.toList
+            |> List.typedPartition (fun (elem, findResult) ->
+                match findResult with
+                | None -> Choice1Of2 elem
+                | Some (rootElem, data) -> Choice2Of2 ({| data = data; rootElem = rootElem |}))
+
+        match newElems, existingElems with
+        | _, [] ->
+            failwith "At least one of the elements in the sequence we're unioning has to already be present in the DSF" // Because we need at least one data to return
+
+        | newEls, existingHead :: existingRest ->
+            let existingNel = NEL.new_ existingHead existingRest
+
+            let minExistingElemAndData = existingNel |> NEL.minBy (fun el -> el.rootElem)
+            let minData = minExistingElemAndData.data
+
+            /// Or to insert for the first time tbh
+            let allElemsToRedirect =
+                NEL.appendList (existingNel |> NEL.map (fun el -> el.rootElem)) newEls
+
+            let minElem = NEL.min<_> allElemsToRedirect
+
+            let newDsf =
+                { dsf with
+                    map =
+                        allElemsToRedirect
+                        |> NEL.fold (fun map elem -> Map.add elem (Pointer minElem) map) dsf.map
+                        |> Map.add minElem (Content minData) }
+
+            existingNel |> NEL.map (fun el -> el.data), minElem, newDsf
+
+
+
+
+    /// @TODO: implement this! might need to implement a `DSFC.unionSetsManySingleStep` method in the process!
+    static member unionSetsMany (elemsToMerge : 'T seq) (dsf : DSFC<'T, 'U>) : DSFC<'T, 'U> =
+        let singleStepResult = DSFC.unionSetsSingleStepMany elemsToMerge dsf
+        let dataToMerge, rootElem, singleStepUnionedDsf = singleStepResult
+
+        let distinctDatas = NEL.distinct<_> dataToMerge
+
+        match distinctDatas with
+        | NEL (onlyData, []) -> { singleStepUnionedDsf with map = dsf.map |> Map.add rootElem (Content onlyData) }
+
+        | _ ->
+            let unifiedData, furtherElsToMerge = dsf.unifyDataMany distinctDatas
+
+            let dsfWithUnifiedData =
+                { singleStepUnionedDsf with map = dsf.map |> Map.add rootElem (Content unifiedData) }
+
+            match Seq.toList furtherElsToMerge with
+            | [] -> dsfWithUnifiedData
+
+            | furtherElsToMerge_ -> DSFC.unionSetsMany furtherElsToMerge_ dsfWithUnifiedData
+
+
+
+    static member private addSetSingleStep
+        (data : 'U)
+        (elemsToMerge : 'T seq)
+        (dsf : DSFC<'T, 'U>)
+        : 'U nel * 'T * DSFC<'T, 'U> =
+        let findResults =
+            elemsToMerge
+            |> Seq.map (fun elem -> elem, DSFC.findRootAndData elem dsf)
+
+        let newElems, existingElems =
+            findResults
+            |> Seq.toList
+            |> List.typedPartition (fun (elem, findResult) ->
+                match findResult with
+                | None -> Choice1Of2 elem
+                | Some (rootElem, data) -> Choice2Of2 ({| data = data; rootElem = rootElem |}))
+
+        match newElems, existingElems with
+        | [], [] -> failwith "There has to be at least one element being added"
+
+        | newElemHead :: newElemTail, [] ->
+            let newEls = NEL.new_ newElemHead newElemTail
+            let minElem = NEL.min<_> newEls
+
+            let newDsf =
+                { dsf with
+                    map =
+                        newEls
+                        |> NEL.fold (fun map elem -> Map.add elem (Pointer minElem) map) dsf.map
+                        |> Map.add minElem (Content data) }
+
+            NEL.make data, minElem, newDsf
+
+
+        | newEls, existingHead :: existingRest ->
+            let existingNel = NEL.new_ existingHead existingRest
+
+            /// Or to insert for the first time tbh
+            let allElemsToRedirect =
+                NEL.appendList (existingNel |> NEL.map (fun el -> el.rootElem)) newEls
+
+            let minElem = NEL.min<_> allElemsToRedirect
+
+            let newDsf =
+                { dsf with
+                    map =
+                        allElemsToRedirect
+                        |> NEL.fold (fun map elem -> Map.add elem (Pointer minElem) map) dsf.map
+                        |> Map.add minElem (Content data) }
+
+            existingNel
+            |> NEL.map (fun el -> el.data)
+            |> NEL.cons data,
+            minElem,
+            newDsf
+
+
+    static member addSet (data : 'U) (elemsToMerge : 'T seq) (dsf : DSFC<'T, 'U>) : DSFC<'T, 'U> =
+        let singleStepResult = DSFC.addSetSingleStep data elemsToMerge dsf
+        let dataToMerge, rootElem, singleStepUnionedDsf = singleStepResult
+
+        let distinctDatas = NEL.distinct<_> dataToMerge
+
+        match distinctDatas with
+        | NEL (onlyData, []) ->
+            { singleStepUnionedDsf with
+                map =
+                    singleStepUnionedDsf.map
+                    |> Map.add rootElem (Content onlyData) }
+
+        | _ ->
+            let unifiedData, furtherElsToMerge = dsf.unifyDataMany distinctDatas
+
+            let dsfWithUnifiedData =
+                { singleStepUnionedDsf with
+                    map =
+                        singleStepUnionedDsf.map
+                        |> Map.add rootElem (Content unifiedData) }
+
+            match Seq.toList furtherElsToMerge with
+            | [] -> dsfWithUnifiedData
+
+            | furtherElsToMerge_ -> DSFC.unionSetsMany furtherElsToMerge_ dsfWithUnifiedData
+
+
+
+and DSFC<'T, 'U when 'T : comparison and 'U : equality> = DisjointSetForestWithContent<'T, 'U>
